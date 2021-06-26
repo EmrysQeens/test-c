4426a31b22d67b69c3486cb172dab9d16e7211b1
emrysqeens <emrysqeens@gmail.com>
2021-06-26 02:29:44 -0700

    add

--- listener.py
+++ listener.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-renderer: Flask = Flask(__name__, static_folder="static", static_url_path="")
+app: Flask = Flask(__name__, static_folder="static", static_url_path="")
 
 
-@renderer.route('/')
+@app.route('/')
 def render():
     return render_template('summer_.html')
 
 
 if __name__ == '__main__':
-    Flask.run(renderer, port=5000, debug=True)
+    Flask.run(app, port=5000, debug=True)
